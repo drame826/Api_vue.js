a01aa2b850073525bbf2075830afaf33ea793ce2
drame826 <dianguineds04@gmail.com>
2023-05-31 18:12:25 +0000

    mack

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Tasks from '../views/Tasks.vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import TodoList from '../views/TodoList.vue'
+import IssueList from '../views/IssueList.vue'
+import TodosIssues from '../views/TodosIssues.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/todo-list',
+    name: 'TodoList',
+    component: TodoList
+  },
+  {
+    path: '/issue-list',
+    name: 'IssueList',
+    component: IssueList
+  },
+  {
+    path: '/todos-issues',
+    name: 'TodosIssues',
+    component: TodosIssues
+  },
+  //Ajouter à partir d'ici
+  {
+    path: '/tasks',
+    name: 'Tasks',
+    component: Tasks
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
